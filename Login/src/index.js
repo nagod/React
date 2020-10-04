67b9a5a0e77dfebd58b8b718aecb5c66e5ba404d
deniz <schulen1994>
2020-10-04 23:11:54 +0200

    added Login with hooks

---
+++ Login/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "./style/index.css"
+import Login from './Components/Login';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Login />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
